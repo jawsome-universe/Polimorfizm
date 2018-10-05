8b6e7a1c4622421bcfbdb9c9142a56066dcdcade
Danylo <34708967+jawsome-universe@users.noreply.github.com>
2018-10-05 14:50:45 +0300

    Update Car.java

--- src/Polimorfizm/Car.java
+++ src/Polimorfizm/Car.java
@@ -2,7 +2,7 @@ package Polimorfizm;
 
 public class Car {
     public void driving(){
-        System.out.println("driving");
+        System.out.println("driving123");
     }
 
     int numOfSeets = 4;
